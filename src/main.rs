4dea3ecf7bbd496051fb62505ee1894938872e0b
Hayden Rose <hayden4r4@gmail.com>
2022-05-07 23:41:12 -0700

    First release, pricing correctly.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use polars::prelude::*;
+use statrs::distribution::{ContinuousCDF, Normal};
+use std::f64::consts::E;
 use std::fs::File;
 
-const STOCK_PRICE: u32 = 50;
-const EXERCISE_PRICE: u32 = 100;
-// Risk-free interest rate
-const R: f32 = 0.02;
-// Time to expiration in days
-const TTE: u32 = 180;
-// Standard deviation of log returns (volatility)
-const STD: f32 = 0.3;
-// Option price
-const OPTION_PRICE: f32 = 0.5;
+#[derive(Clone)]
+enum OptionType {
+    Call,
+    Put,
+}
+
+struct Inputs {
+    option_type: OptionType,
+    // Stock price
+    s: f64,
+    // Strike price
+    k: f64,
+    // risk-free rate
+    r: f64,
+    // time to maturity as fraction of year
+    t: f64,
+    // volatility
+    sigma: f64,
+}
 
 fn load_returns() -> Result<DataFrame> {
+    // Loads return data from csv file
     let file = File::open("SP500_returns.csv")?;
 
     CsvReader::new(file)
@@ -21,14 +33,84 @@ fn load_returns() -> Result<DataFrame> {
         .finish()
 }
 
-fn log_return(returns_df: DataFrame) -> f32 {
-    let df: DataFrame = returns_df;
-    let close_series:Series = df["Close"].clone();
-    let log_return: f32 = close_series.str_value(0).to_string().parse::<f32>().unwrap()/close_series.str_value(close_series.len()-1).to_string().parse::<f32>().unwrap().ln() / close_series.len() as f32;
-    log_return
+fn std_dev_log_return(returns: Series) -> f64 {
+    // Returns the std dev of log returns
+    // Calculates the standard deviation of log returns
+    let mut returns_vec: Vec<f64> = Vec::new();
+    let mut mean: f64 = 0.0;
+    for i in 0..returns.len() {
+        if i <= returns.len() - 2 {
+            returns_vec.push(
+                (returns.str_value(i + 1).to_string().parse::<f64>().unwrap()
+                    / returns.str_value(i).to_string().parse::<f64>().unwrap())
+                .ln(),
+            );
+            mean += returns_vec[i];
+        }
+    }
+
+    mean /= returns_vec.len() as f64;
+    // Calculating variance of log returns
+    let mut variance: f64 = 0.0;
+    for i in 0..returns_vec.len() {
+        variance += (returns_vec[i] - mean).powi(2);
+    }
+    variance /= returns_vec.len() as f64;
+    // Returns square root of variance = std dev.
+    variance.sqrt()
+}
+
+fn nd1nd2(inputs: &Inputs) -> (f64, f64) {
+    // Returns the first and second order moments of the normal distribution
+
+    // Calculating numerator of the first moment of the normal distribution
+    let numd1: f64 =
+        (inputs.s / inputs.k).ln() + (inputs.r + (inputs.sigma.powi(2)) / 2.0) * inputs.t;
+    // Calculating numerator of the second moment of the normal distribution
+    let numd2: f64 =
+        (inputs.s / inputs.k).ln() + (inputs.r - (inputs.sigma.powi(2)) / 2.0) * inputs.t;
+    // Calculating denominator of the first and second moment of the normal distribution
+    let den: f64 = inputs.sigma * (inputs.t.sqrt());
+    let d1d2: (f64, f64) = (numd1 / den, numd2 / den);
+    // Creating normal distribution
+    let n: Normal = Normal::new(0.0, 1.0).unwrap();
+    // Calculates the first and second order moments of the normal distribution
+    let nd1nd2: (f64, f64) = match inputs.option_type {
+        OptionType::Call => (n.cdf(d1d2.0), n.cdf(d1d2.1)),
+        OptionType::Put => (n.cdf(-d1d2.0), n.cdf(-d1d2.1)),
+    };
+    nd1nd2
+}
+
+fn calc_price(inputs: &Inputs) -> f64 {
+    // Returns the price of the option
+
+    // Calculates the price of the option
+    let (nd1, nd2): (f64, f64) = nd1nd2(inputs);
+    let price: f64 = match inputs.option_type {
+        OptionType::Call => f64::max(
+            0.0,
+            nd1 * inputs.s - nd2 * inputs.k * E.powf(-inputs.r * inputs.t),
+        ),
+        OptionType::Put => f64::max(
+            0.0,
+            nd2 * inputs.k * E.powf(-inputs.r * inputs.t) - nd1 * inputs.s,
+        ),
+    };
+    price
 }
 
 fn main() {
-    let returns_df: DataFrame = load_returns().unwrap();
-    println!("{}", log_return(returns_df));
+    let returns: Series = load_returns().unwrap()["Close"].clone();
+    let std_dev: f64 = std_dev_log_return(returns);
+    let inputs: Inputs = Inputs {
+        option_type: OptionType::Call,
+        s: 4123.34,
+        k: 4120.0,
+        r: 0.0312,
+        t: 176.0 / 360.0,
+        sigma: std_dev,
+    };
+    let price: f64 = calc_price(&inputs);
+    println!("{}", price);
 }
