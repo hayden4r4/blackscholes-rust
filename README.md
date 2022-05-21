fc2fda47305308bd6468d9b1be9b9bc9d1125307
Hayden Rose <hayden4r4@gmail.com>
2022-05-20 19:54:08 -0700

    Added support for dividends

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Black-Scholes Rust
 
-A Black-Scholes pricing model built in Rust. This is a basic Black-Scholes implementation assuming no dividends and a European option, will calculate the price of calls or puts. The computation is parallelized using crossbeam. Simply pass an Inputs struct to the calc_price() function to return the calculated price of an option.
+A Black-Scholes pricing model built in Rust. Will calculate the price of calls or puts. The computation is parallelized using crossbeam. Simply pass an Inputs struct to the calc_price() function to return the calculated price of an option.
--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@ pub struct Inputs {
     pub k: f64,
     // risk-free rate
     pub r: f64,
+    // dividend yield
+    pub q: f64,
     // time to maturity as fraction of year
     pub t: f64,
     // volatility
@@ -29,6 +31,7 @@ fn nd1nd2(inputs: Arc<RwLock<Inputs>>) -> (f64, f64) {
 
     // Creating a vector to hold threads before joining
     let mut handles = Vec::new();
+
     // Creating a channel with max capacity of one message to communicate between threads
     let (numd1_tx, numd1_rx) = bounded(1);
     // Creating mutex for inputs
@@ -38,30 +41,21 @@ fn nd1nd2(inputs: Arc<RwLock<Inputs>>) -> (f64, f64) {
         // Calculating numerator of the first moment of the normal distribution
         // Obtains a read lock on inputs1
         let inputs = inputs1.read().unwrap();
-        let numd1: f64 =
-            (inputs.s / inputs.k).ln() + (inputs.r + (inputs.sigma.powi(2)) / 2.0) * inputs.t;
+
+        let numd1: f64 = (inputs.s / inputs.k).ln()
+            + (inputs.r - inputs.q + (inputs.sigma.powi(2)) / 2.0) * inputs.t;
         // Send the result to the channel
         numd1_tx.send(numd1).unwrap();
     });
     // Pushing the handle to the handles vector
     handles.push(handle_numd1);
 
-    let (numd2_tx, numd2_rx) = bounded(1);
-    let inputs2: Arc<RwLock<Inputs>> = Arc::clone(&inputs);
-    let handle_numd2 = thread::spawn(move || {
-        // Calculating numerator of the second moment of the normal distribution
-        let inputs = inputs2.read().unwrap();
-        let numd2: f64 =
-            (inputs.s / inputs.k).ln() + (inputs.r - (inputs.sigma.powi(2)) / 2.0) * inputs.t;
-        numd2_tx.send(numd2).unwrap();
-    });
-    handles.push(handle_numd2);
-
     let (den_tx, den_rx) = bounded(1);
-    let inputs3: Arc<RwLock<Inputs>> = Arc::clone(&inputs);
+    let inputs2: Arc<RwLock<Inputs>> = Arc::clone(&inputs);
     let handle_den = thread::spawn(move || {
         // Calculating denominator of the first and second moment of the normal distribution
-        let inputs = inputs3.read().unwrap();
+        let inputs = inputs2.read().unwrap();
+
         let den: f64 = inputs.sigma * (inputs.t.sqrt());
         den_tx.send(den).unwrap();
     });
@@ -71,10 +65,11 @@ fn nd1nd2(inputs: Arc<RwLock<Inputs>>) -> (f64, f64) {
     let handle_d1d2 = thread::spawn(move || {
         // Calculating the first and second moment of the normal distribution
         let den: f64 = den_rx.recv().unwrap();
-        let d1d2: (f64, f64) = (
-            numd1_rx.recv().unwrap() / den,
-            numd2_rx.recv().unwrap() / den,
-        );
+
+        let d1: f64 = numd1_rx.recv().unwrap() / den;
+        let d2: f64 = d1 - den;
+
+        let d1d2: (f64, f64) = (d1, d2);
         d1d2_tx.send(d1d2).unwrap();
     });
     handles.push(handle_d1d2);
@@ -82,6 +77,7 @@ fn nd1nd2(inputs: Arc<RwLock<Inputs>>) -> (f64, f64) {
     let (n_tx, n_rx) = bounded(1);
     let handle_n = thread::spawn(move || {
         // Creating normal distribution
+
         let n: Normal = Normal::new(0.0, 1.0).unwrap();
         n_tx.send(n).unwrap();
     });
@@ -89,7 +85,9 @@ fn nd1nd2(inputs: Arc<RwLock<Inputs>>) -> (f64, f64) {
 
     // Calculates the first and second order moments of the normal distribution
     let n: Normal = n_rx.recv().unwrap();
+
     let d1d2: (f64, f64) = d1d2_rx.recv().unwrap();
+
     // Checks if OptionType is Call or Put
     let nd1nd2: (f64, f64) = match inputs.read().unwrap().option_type {
         OptionType::Call => (n.cdf(d1d2.0), n.cdf(d1d2.1)),
@@ -113,11 +111,13 @@ pub fn calc_price(inputs: Inputs) -> f64 {
     let price: f64 = match inputs.option_type {
         OptionType::Call => f64::max(
             0.0,
-            nd1 * inputs.s - nd2 * inputs.k * E.powf(-inputs.r * inputs.t),
+            nd1 * inputs.s * E.powf(-inputs.q * inputs.t)
+                - nd2 * inputs.k * E.powf(-inputs.r * inputs.t),
         ),
         OptionType::Put => f64::max(
             0.0,
-            nd2 * inputs.k * E.powf(-inputs.r * inputs.t) - nd1 * inputs.s,
+            nd2 * inputs.k * E.powf(-inputs.r * inputs.t)
+                - nd1 * inputs.s * E.powf(-inputs.q * inputs.t),
         ),
     };
     price
