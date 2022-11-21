7da89eb4ea625721737349c416b1d0e037e30c70
Hayden Rose <hayden4r4@gmail.com>
2022-11-20 17:50:41 -0800

    added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! # blackscholes
+//! This library provides an simple, lightweight, and efficient (though not heavily optimized) implementation of the Black-Scholes-Merton model for pricing European options.
+//!
+//! ## Usage
+//! Simply create an instance of the `Inputs` struct and call the desired method.
+//!
+//! Example:
+//! ```
+//! let inputs: blackscholes::Inputs = blackscholes::Inputs.new(blackscholes::OptionType::Call, 100.0, 100.0, None, 0.05, 0.02, 20.0 / 365.25, Some(0.2));
+//! let price: f64 = inputs.calc_price();
+//! ```
+//!
+//! See the [Github Repo](https://github.com/hayden4r4/blackscholes-rust/tree/python_package) for full source code.  Other implementations such as a WASM crate and a [pure Rust Crate](https://crates.io/crates/blackscholes) are also available.
+
 use statrs::distribution::{Continuous, ContinuousCDF, Normal};
 use std::f64::consts::{E, PI};
 use std::fmt::{Display, Formatter, Result};
@@ -11,6 +25,7 @@ fn blackscholes_python(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
+/// The type of option to be priced. Call or Put.
 #[derive(Debug, Clone, Eq, PartialEq)]
 #[pyclass(text_signature = "(Call, Put, /)")]
 pub enum OptionType {
@@ -29,6 +44,16 @@ impl Display for OptionType {
 
 #[pymethods]
 impl OptionType {
+    /// Creates an instance of the `OptionType` enum.
+    /// # Arguments
+    /// * `option_type` - The type of option to be priced. Call or Put.
+    /// # Example
+    /// ```
+    /// use blackscholes::OptionType;
+    /// let option_type: OptionType = OptionType::Call;
+    /// ```
+    /// # Returns
+    /// An instance of the `OptionType` enum.
     #[new]
     pub fn new(option_type: &str) -> Self {
         match option_type {
@@ -37,29 +62,32 @@ impl OptionType {
             _ => panic!("Option type must be either Call or Put"),
         }
     }
+    /// # Returns
+    /// A string representation of the `OptionType` enum.
     pub fn __str__(&self) -> String {
         format!("{}", self)
     }
 }
 
+/// The inputs to the Black-Scholes-Merton model.
 #[derive(Debug, Clone)]
 #[pyclass(text_signature = "(option_type, s, k, p, r, q, t, sigma, /)")]
 pub struct Inputs {
-    // The type of the option (call or put)
+    /// The type of the option (call or put)
     pub option_type: OptionType,
-    // Stock price
+    /// Stock price
     pub s: f64,
-    // Strike price
+    /// Strike price
     pub k: f64,
-    // Option price
+    /// Option price
     pub p: Option<f64>,
-    // Risk-free rate
+    /// Risk-free rate
     pub r: f64,
-    // Dividend yield
+    /// Dividend yield
     pub q: f64,
-    // Time to maturity as fraction of year
+    /// Time to maturity in years
     pub t: f64,
-    // Volatility
+    /// Volatility
     pub sigma: Option<f64>,
 }
 
@@ -83,9 +111,10 @@ impl Display for Inputs {
     }
 }
 
+/// Calculates the d1, d2, nd1, and nd2 values for the option.
+/// # Returns
+/// Tuple (f64, f64) of the nd1 and nd2 values for the given inputs.
 fn nd1nd2(inputs: &Inputs, normal: bool) -> (f64, f64) {
-    // Returns the nd1 and nd2 values for the given inputs
-
     let sigma: f64 = match inputs.sigma {
         Some(sigma) => sigma,
         None => panic!("Expected an Option(f64) for inputs.sigma, received None"),
@@ -124,9 +153,9 @@ fn nd1nd2(inputs: &Inputs, normal: bool) -> (f64, f64) {
     nd1nd2
 }
 
+/// # Returns
+/// f64 of the derivative of the nd1.
 fn calc_nprimed1(inputs: &Inputs) -> f64 {
-    // Returns the derivative of the nd1
-
     let (d1, _): (f64, f64) = nd1nd2(&inputs, false);
 
     // Generate normal probability distribution
@@ -137,8 +166,26 @@ fn calc_nprimed1(inputs: &Inputs) -> f64 {
     nprimed1
 }
 
+/// Methods for calculating the price, greeks, and implied volatility of an option.
 #[pymethods]
 impl Inputs {
+    /// Creates instance ot the `Inputs` struct.
+    /// # Arguments
+    /// * `option_type` - The type of option to be priced.
+    /// * `s` - The current price of the underlying asset.
+    /// * `k` - The strike price of the option.
+    /// * `p` - The dividend yield of the underlying asset.
+    /// * `r` - The risk-free interest rate.
+    /// * `q` - The dividend yield of the underlying asset.
+    /// * `t` - The time to maturity of the option in years.
+    /// * `sigma` - The volatility of the underlying asset.
+    /// # Example
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20/365.25, Some(0.2));
+    /// ```
+    /// # Returns
+    /// An instance of the `Inputs` struct.
     #[new]
     pub fn new(
         option_type: OptionType,
@@ -162,6 +209,8 @@ impl Inputs {
         }
     }
 
+    /// # Returns
+    /// string representation of the `Inputs` struct.    
     pub fn __str__(&self) -> String {
         format!(
             "OptionType: {}, S: {}, K: {}, P: {}, R: {}, Q: {}, T: {}, Sigma: {}",
@@ -181,11 +230,18 @@ impl Inputs {
             },
         )
     }
+    /// Calculates the price of the option.
+    /// # Requires
+    /// s, k, r, q, t, sigma.
+    /// # Returns
+    /// f64 of the price of the option.
+    /// # Example
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20/365.25, Some(0.2));
+    /// let price = inputs.calc_price();
+    /// ```
     pub fn calc_price(&self) -> PyResult<f64> {
-        // Returns the price of the option
-        // Requires s k r q t sigma
-
-        // Calculates the price of the option
         let (nd1, nd2): (f64, f64) = nd1nd2(self, true);
         let price: f64 = match self.option_type {
             OptionType::Call => f64::max(
@@ -199,10 +255,18 @@ impl Inputs {
         };
         Ok(price)
     }
-    // Requires s k r q t sigma
+    /// Calculates the delta of the option.
+    /// # Requires
+    /// s, k, r, q, t, sigma
+    /// # Returns
+    /// f64 of the delta of the option.
+    /// # Example
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20/365.25, Some(0.2));
+    /// let delta = inputs.calc_delta();
+    /// ```
     pub fn calc_delta(&self) -> PyResult<f64> {
-        // Calculates the delta of the option
-
         let (nd1, _): (f64, f64) = nd1nd2(self, true);
         let delta: f64 = match self.option_type {
             OptionType::Call => nd1 * E.powf(-self.q * self.t),
@@ -211,9 +275,18 @@ impl Inputs {
         Ok(delta)
     }
 
+    /// Calculates the gamma of the option.
+    /// # Requires
+    /// s, k, r, q, t, sigma
+    /// # Returns
+    /// f64 of the gamma of the option.
+    /// # Example
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20/365.25, Some(0.2));
+    /// let gamma = inputs.calc_gamma();
+    /// ```
     pub fn calc_gamma(&self) -> PyResult<f64> {
-        // Calculates the gamma of the option
-
         let sigma: f64 = match self.sigma {
             Some(sigma) => sigma,
             None => panic!("Expected an Option(f64) for self.sigma, received None"),
@@ -224,9 +297,19 @@ impl Inputs {
         Ok(gamma)
     }
 
+    /// Calculates the theta of the option.
+    /// Uses 365.25 days in a year for calculations.
+    /// # Requires
+    /// s, k, r, q, t, sigma
+    /// # Returns
+    /// f64 of theta per day (not per year).
+    /// # Example
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20/365.25, Some(0.2));
+    /// let theta = inputs.calc_theta();
+    /// ```
     pub fn calc_theta(&self) -> PyResult<f64> {
-        // Calculates the theta of the option
-
         let sigma: f64 = match self.sigma {
             Some(sigma) => sigma,
             None => panic!("Expected an Option(f64) for self.sigma, received None"),
@@ -253,17 +336,35 @@ impl Inputs {
         Ok(theta)
     }
 
+    /// Calculates the vega of the option.
+    /// # Requires
+    /// s, k, r, q, t, sigma
+    /// # Returns
+    /// f64 of the vega of the option.
+    /// # Example
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20/365.25, Some(0.2));
+    /// let vega = inputs.calc_vega();
+    /// ```
     pub fn calc_vega(&self) -> PyResult<f64> {
-        // Calculates the vega of the option
-
         let nprimed1: f64 = calc_nprimed1(self);
         let vega: f64 = 1.0 / 100.0 * self.s * E.powf(-self.q * self.t) * self.t.sqrt() * nprimed1;
         Ok(vega)
     }
 
+    /// Calculates the rho of the option.
+    /// # Requires
+    /// s, k, r, q, t, sigma
+    /// # Returns
+    /// f64 of the rho of the option.
+    /// # Example
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20/365.25, Some(0.2));
+    /// let rho = inputs.calc_rho();
+    /// ```
     pub fn calc_rho(&self) -> PyResult<f64> {
-        // Calculates the rho of the option
-
         let (_, nd2): (f64, f64) = nd1nd2(self, true);
         let rho: f64 = match self.option_type {
             OptionType::Call => 1.0 / 100.0 * self.k * self.t * E.powf(-self.r * self.t) * nd2,
@@ -272,13 +373,23 @@ impl Inputs {
         Ok(rho)
     }
 
+    /// Calculates the implied volatility of the option.
+    /// Tolerance is the max error allowed for the implied volatility,
+    /// the lower the tolerance the more iterations will be required.
+    /// Recommended to be a value between 0.001 - 0.0001 for highest efficiency/accuracy.
+    /// Initializes estimation of sigma using Brenn and Subrahmanyam (1998) method of calculating initial iv estimation.
+    /// Uses Newton Raphson algorithm to calculate implied volatility.
+    /// # Requires
+    /// s, k, r, q, t, p
+    /// # Returns
+    /// f64 of the implied volatility of the option.
+    /// # Example:
+    /// ```
+    /// use blackscholes::Inputs;
+    /// let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, Some(10), 0.05, 0.02, 20.0 / 365.25, None);
+    /// let iv = inputs.calc_iv(0.0001);
+    /// ```
     pub fn calc_iv(&self, tolerance: f64) -> PyResult<f64> {
-        // Calculates the implied volatility of the option
-        // Tolerance is the max error allowed for the implied volatility,
-        // the lower the tolerance the more iterations will be required.
-        // Recommended to be a value between 0.001 - 0.0001 for highest efficiency/accuracy
-        // Requires s k r q t price
-
         let mut inputs: Inputs = self.clone();
 
         let p: f64 = match inputs.p {
