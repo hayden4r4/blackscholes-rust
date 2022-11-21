77c1ebafead8518c8b7fdab29cb072f9c031d16f
Hayden Rose <hayden4r4@gmail.com>
2022-11-20 18:50:04 -0800

    added npm link to docs

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //! let price: f64 = inputs.calc_price();
 //! ```
 //!
-//! See the [Github Repo](https://github.com/hayden4r4/blackscholes-rust/tree/python_package) for full source code.  Other implementations such as a WASM crate and a [pure Rust Crate](https://crates.io/crates/blackscholes) are also available.
+//! See the [Github Repo](https://github.com/hayden4r4/blackscholes-rust/tree/python_package) for full source code.  Other implementations such as a [npm WASM package](https://www.npmjs.com/package/@haydenr4/blackscholes_wasm) and a [pure Rust Crate](https://crates.io/crates/blackscholes) are also available.
 
 use statrs::distribution::{Continuous, ContinuousCDF, Normal};
 use std::f64::consts::{E, PI};
