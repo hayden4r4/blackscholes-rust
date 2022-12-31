79159a53dda3b58abff1591456386558adf8fd11
Hayden Rose <hayden4r4@gmail.com>
2022-12-31 15:00:39 -0800

    added beginning of integration tests

---
+++ tests/bstest.rs
@@ -0,0 +1,60 @@
+use blackscholes;
+
+const INPUTS_CALL_OTM: blackscholes::Inputs = blackscholes::Inputs {
+    option_type: blackscholes::OptionType::Call,
+    s: 100.0,
+    k: 110.0,
+    p: None,
+    r: 0.05,
+    q: 0.05,
+    t: 20.0 / 365.25,
+    sigma: Some(0.2),
+};
+const INPUTS_CALL_ITM: blackscholes::Inputs = blackscholes::Inputs {
+    option_type: blackscholes::OptionType::Call,
+    s: 100.0,
+    k: 90.0,
+    p: None,
+    r: 0.05,
+    q: 0.05,
+    t: 20.0 / 365.25,
+    sigma: Some(0.2),
+};
+const INPUTS_PUT_OTM: blackscholes::Inputs = blackscholes::Inputs {
+    option_type: blackscholes::OptionType::Call,
+    s: 100.0,
+    k: 90.0,
+    p: None,
+    r: 0.05,
+    q: 0.05,
+    t: 20.0 / 365.25,
+    sigma: Some(0.2),
+};
+const INPUTS_PUT_ITM: blackscholes::Inputs = blackscholes::Inputs {
+    option_type: blackscholes::OptionType::Call,
+    s: 100.0,
+    k: 110.0,
+    p: None,
+    r: 0.05,
+    q: 0.05,
+    t: 20.0 / 365.25,
+    sigma: Some(0.2),
+};
+
+#[test]
+fn price_call_otm() {
+    assert!((INPUTS_CALL_OTM.calc_price() - 0.0376).abs() < 0.001);
+}
+#[test]
+fn price_call_itm() {
+    assert!((INPUTS_CALL_ITM.calc_price() - 9.9913).abs() < 0.001);
+}
+
+#[test]
+fn price_put_otm() {
+    assert!((INPUTS_PUT_OTM.calc_price() - 0.01867).abs() < 0.001);
+}
+#[test]
+fn price_put_itm() {
+    assert!((INPUTS_PUT_ITM.calc_price() - 10.0103).abs() < 0.001);
+}
