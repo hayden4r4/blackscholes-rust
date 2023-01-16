c59b4b7b23e1992898b3ee515e305a3bdfcbbb21
Hayden Rose <hayden4r4@gmail.com>
2023-01-16 01:02:34 -0800

    changed a doc

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //! Example:
 //! ```
-//! use blackscholes::{Inputs, OptionType, pricing::Pricing};
+//! use blackscholes::{Inputs, OptionType, Pricing};
 //! let inputs = Inputs::new(OptionType::Call, 100.0, 100.0, None, 0.05, 0.2, 20.0/365.25, Some(0.2));
 //! let price: f32 = inputs.calc_price().unwrap();
 //! ```
