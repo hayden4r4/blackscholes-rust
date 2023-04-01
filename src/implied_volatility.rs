1ad17e96bb31137f6653d940a334290682e06eed
Hayden Rose <hayden4r4@gmail.com>
2023-03-31 18:23:11 -0700

    fixed iv error checking and cleaned up docstrings

--- src/implied_volatility.rs
+++ src/implied_volatility.rs
@@ -38,17 +38,8 @@ impl ImpliedVolatility<f32> for Inputs {
     /// let iv = inputs.calc_iv(0.0001).unwrap();
     /// ```
     /// Initial estimation of sigma using Modified Corrado-Miller from ["A MODIFIED CORRADO-MILLER IMPLIED VOLATILITY ESTIMATOR" (2007) by Piotr P√luciennik](https://sin.put.poznan.pl/files/download/37938) method of calculating initial iv estimation.
-    /// Note: While this method is more accurate than Brenn and Subrahmanyam (1998) it will still sometimes fail to converge.
-    /// An example of failure to converge:
-    /// ```should_panic
-    /// use blackscholes::{Inputs, OptionType, ImpliedVolatility};
-    /// let inputs = Inputs::new(OptionType::Call, 105.0, 100.0, Some(30.0), 0.05, 0.05, 30.0/365.25, None);
-    /// // This will fail to converge, the NaN sigma value is checked in the function and will return an error.
-    /// assert_eq!(inputs.calc_iv(0.0001).is_err(), true);
-    /// ```
-    ///
     /// A more accurate method is the "Let's be rational" method from ["Let’s be rational" (2016) by Peter Jackel](http://www.jaeckel.org/LetsBeRational.pdf)
-    /// however this method is much more complicated, it is available as calc_rational_iv() in the RationalImpliedVolatility trait.
+    /// however this method is much more complicated, it is available as calc_rational_iv().
     #[allow(non_snake_case)]
     fn calc_iv(&self, tolerance: f32) -> Result<f32, String> {
         let mut inputs: Inputs = self.clone();
@@ -57,7 +48,7 @@ impl ImpliedVolatility<f32> for Inputs {
             .p
             .ok_or("inputs.p must contain Some(f32), found None".to_string())?;
         // Initialize estimation of sigma using Brenn and Subrahmanyam (1998) method of calculating initial iv estimation.
-        // Note: I have since found that this method is not accurate enough which will sometimes cause the algorithm to fail to converge, hence I have commented it out.
+        // commented out to replace with modified corrado-miller method.
         // let mut sigma: f32 = (PI2 / inputs.t).sqrt() * (p / inputs.s);
 
         let X: f32 = inputs.k * E.powf(-inputs.r * inputs.t);
@@ -82,6 +73,7 @@ impl ImpliedVolatility<f32> for Inputs {
         if sigma.is_nan() {
             Err("Failed to converge".to_string())?
         }
+
         // Initialize diff to 100 for use in while loop
         let mut diff: f32 = 100.0;
 
@@ -92,6 +84,10 @@ impl ImpliedVolatility<f32> for Inputs {
             inputs.sigma = Some(sigma);
             diff = Inputs::calc_price(&inputs)? - p;
             sigma -= diff / (Inputs::calc_vega(&inputs)? * 100.0);
+
+            if sigma.is_nan() || sigma.is_infinite() {
+                Err("Failed to converge".to_string())?
+            }
         }
         Ok(sigma)
     }
