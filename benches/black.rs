6d314d7960e0213be7753a1010bb2f024be4b0b7
Przemyslaw Olszewski <przem.olszewski@outlook.com>
2024-07-17 23:53:38 +0200

    fmt: fixes

--- benches/black.rs
+++ benches/black.rs
@@ -1,5 +1,9 @@
 extern crate criterion;
-use blackscholes::lets_be_rational::black::{asymptotic_expansion_of_normalised_black_call, asymptotic_expansion_of_normalised_black_call_old, small_t_expansion_of_normalised_black_call, small_t_expansion_of_normalised_black_call_old};
+use blackscholes::lets_be_rational::black::{
+    asymptotic_expansion_of_normalised_black_call,
+    asymptotic_expansion_of_normalised_black_call_old, small_t_expansion_of_normalised_black_call,
+    small_t_expansion_of_normalised_black_call_old,
+};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn benchmark_asymptotic_expansion_of_normalised_black_call_changed(c: &mut Criterion) {
@@ -48,7 +52,6 @@ fn benchmark_asymptotic_expansion_of_normalised_black_call(c: &mut Criterion) {
     }
 }
 
-
 fn benchmark_small_t_expansion_old(c: &mut Criterion) {
     let test_values = vec![
         (0.1, 0.1),
@@ -60,8 +63,15 @@ fn benchmark_small_t_expansion_old(c: &mut Criterion) {
 
     for (h, t) in test_values {
         c.bench_function(
-            &format!("small_t_expansion_of_normalised_black_call_old h={}, t={}", h, t),
-            |b| b.iter(|| small_t_expansion_of_normalised_black_call_old(black_box(h), black_box(t))),
+            &format!(
+                "small_t_expansion_of_normalised_black_call_old h={}, t={}",
+                h, t
+            ),
+            |b| {
+                b.iter(|| {
+                    small_t_expansion_of_normalised_black_call_old(black_box(h), black_box(t))
+                })
+            },
         );
     }
 }
@@ -77,17 +87,19 @@ fn benchmark_small_t_expansion_new(c: &mut Criterion) {
 
     for (h, t) in test_values {
         c.bench_function(
-            &format!("small_t_expansion_of_normalised_black_call_new h={}, t={}", h, t),
+            &format!(
+                "small_t_expansion_of_normalised_black_call_new h={}, t={}",
+                h, t
+            ),
             |b| b.iter(|| small_t_expansion_of_normalised_black_call(black_box(h), black_box(t))),
         );
     }
 }
 
-
 criterion_group!(
     benches,
-    // benchmark_asymptotic_expansion_of_normalised_black_call_changed,
-    // benchmark_asymptotic_expansion_of_normalised_black_call,
+    benchmark_asymptotic_expansion_of_normalised_black_call_changed,
+    benchmark_asymptotic_expansion_of_normalised_black_call,
     benchmark_small_t_expansion_old,
     benchmark_small_t_expansion_new
 );
