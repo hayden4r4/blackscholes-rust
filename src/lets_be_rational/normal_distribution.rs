6ef0cfbae9bb3c834a29cd36684ebf8c6d17334b
Claire Ratigan <cratigan@proton.me>
2024-07-26 13:50:08 -0500

    re-introduce statrs methods without static calls

--- src/lets_be_rational/normal_distribution.rs
+++ src/lets_be_rational/normal_distribution.rs
@@ -1,21 +1,11 @@
-use statrs::{consts::SQRT_2PI, function::erf};
-use std::f64::consts::SQRT_2;
+use statrs::distribution::{Continuous, ContinuousCDF, Normal};
 
-const MEAN: f64 = 0.0;
-const STD_DEV: f64 = 1.0;
-
-// Re-implementations of statrs::distribution::Normal::standard()
-// with const values for compiler optimization
 pub fn standard_normal_cdf(x: f64) -> f64 {
-    0.5 * erf::erfc((MEAN - x) / (STD_DEV * SQRT_2))
+    Normal::standard().cdf(x)
 }
 
 pub fn inverse_normal_cdf(x: f64) -> f64 {
-    if !(0.0..=1.0).contains(&x) {
-        panic!("x must be in [0, 1]");
-    } else {
-        MEAN - (STD_DEV * SQRT_2 * erf::erfc_inv(2.0 * x))
-    }
+    Normal::standard().inverse_cdf(x)
 }
 
 pub fn inverse_f_upper_map(f: f64) -> f64 {
@@ -23,6 +13,5 @@ pub fn inverse_f_upper_map(f: f64) -> f64 {
 }
 
 pub fn standard_normal_pdf(x: f64) -> f64 {
-    let d = (x - MEAN) / STD_DEV;
-    (-0.5 * d * d).exp() / (SQRT_2PI * STD_DEV)
+    Normal::standard().pdf(x)
 }
