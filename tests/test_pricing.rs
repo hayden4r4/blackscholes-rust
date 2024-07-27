6cef190aa83c293c802ffead85f9d02ae10f323e
Przemyslaw Olszewski <przem.olszewski@outlook.com>
2024-07-27 05:47:04 +0200

    fix: approx instead of assert

--- tests/test_pricing.rs
+++ tests/test_pricing.rs
@@ -45,16 +45,16 @@ const INPUTS_PUT_ITM: Inputs = Inputs {
 
 #[test]
 fn price_call_otm() {
-    assert!((INPUTS_CALL_OTM.calc_price().unwrap() - 0.0376).abs() < 0.001);
+    assert_approx_eq!(INPUTS_CALL_OTM.calc_price().unwrap(), 0.0376, 0.001);
 }
 #[test]
 fn price_call_itm() {
-    assert!((INPUTS_CALL_ITM.calc_price().unwrap() - 9.9913).abs() < 0.001);
+    assert_approx_eq!(INPUTS_CALL_ITM.calc_price().unwrap(), 9.9913, 0.001);
 }
 
 #[test]
 fn price_put_otm() {
-    assert!((INPUTS_PUT_OTM.calc_price().unwrap() - 0.01867).abs() < 0.001);
+    assert_approx_eq!(INPUTS_PUT_OTM.calc_price().unwrap(), 0.01867, 0.001);
 }
 #[test]
 fn price_put_itm() {
@@ -65,19 +65,19 @@ fn price_put_itm() {
 fn price_using_lets_be_rational() {
     // compare the results from calc_price() and calc_rational_price() for the options above
     assert_approx_eq!(
-        INPUTS_CALL_OTM.calc_price().unwrap() as f64,
+        INPUTS_CALL_OTM.calc_price().unwrap(),
         INPUTS_CALL_OTM.calc_rational_price().unwrap(),
         0.001
     );
 
     assert_approx_eq!(
-        INPUTS_CALL_ITM.calc_price().unwrap() as f64,
+        INPUTS_CALL_ITM.calc_price().unwrap(),
         INPUTS_CALL_ITM.calc_rational_price().unwrap(),
         0.001
     );
 
     assert_approx_eq!(
-        INPUTS_PUT_OTM.calc_price().unwrap() as f64,
+        INPUTS_PUT_OTM.calc_price().unwrap(),
         INPUTS_PUT_OTM.calc_rational_price().unwrap(),
         0.001
     );
